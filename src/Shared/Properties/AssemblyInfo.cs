1e7b1192920b4c3e0d6e6df34b4f8de6914e3b7a
Anton Mednonogov <anton.mednonogov@ansarada.com>
2020-03-14 13:47:33 +0700

    Setup GitVersion

---
+++ src/Shared/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿/* =========================================================================== *
+ *                                                                             *
+ *    The file contains assembly attributes and other miscellaneous things.    *
+ *                                                                             *
+ * =========================================================================== */
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.1.17.0")]
+[assembly: AssemblyFileVersion("0.1.17.0")]
+[assembly: AssemblyInformationalVersion("0.1.17+Branch.master.Sha.a5805cb5c7bf8cfe8957ad432b62f206692a0bae")]
+
