72cffdefaafa6f07ddd422281c4d7f139e1eebf0
Anton Mednonogov <anton.mednonogov@ansarada.com>
2020-04-05 18:57:14 +0700

    Change known properties names

--- src/Domainator.Aws.Infrastructure/Repositories/StateManagement/Storage/KnownTableAttributes.cs
+++ src/Domainator.Aws.Infrastructure/Repositories/StateManagement/Storage/KnownTableAttributes.cs
@@ -5,9 +5,9 @@ namespace Domainator.Infrastructure.Repositories.StateManagement.Storage
         public static readonly string PartitionKey = "PK";
         public static readonly string SortKey = "SK";
         public static readonly string Data = "Data";
-        public static readonly string Version = "Ver";
+        public static readonly string Version = "Version";
         public static readonly string AggregateId = "Id";
-        public static readonly string AggregateType = "Tag";
+        public static readonly string AggregateType = "Type";
         public static readonly string CreatedAt = "CreatedAt";
         public static readonly string UpdatedAt = "UpdatedAt";
         public static readonly string LastChangeSet = "LastChangeSet";
